d8a673422ff783d630db3fe3dc579fc91c6a7591
joaombvb <joaombvb@gmail.com>
2024-07-30 17:31:31 -0300

    day2

--- app.js
+++ app.js
@@ -6,16 +6,49 @@ function sortear() {
     let sorteados = [];
     let numero; 
 
-    for (let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatorio(de, ate);
-        sorteados.push(numero);
-    }
 
-    let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    if (quantidade < (ate - de)) {
+        for (let i = 0; i < quantidade; i++) {
+            numero = obterNumeroAleatorio(de, ate);
+    
+            while (sorteados.includes(numero)) {
+                numero = obterNumeroAleatorio(de, ate);
+            }
+            
+            sorteados.push(numero);
+        }
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+        alterarStatusBotao();
+    } else {
+        alert('ERRO: A quantidade de números para serem sorteados é maior que o intervalo de números escolhido. Por favor, tente novamente!');
+    }
     
 }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 } 
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+    alterarStatusBotao();    
+}
